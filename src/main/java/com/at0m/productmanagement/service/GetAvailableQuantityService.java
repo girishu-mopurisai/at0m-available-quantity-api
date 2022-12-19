7dda91ec33b0be78827846dc68b178bbbb83d189
Girishu M S <girishums1999@gmail.com>
2022-12-19 19:26:09 +0530

    Added two more endpoints

--- src/main/java/com/at0m/productmanagement/service/GetAvailableQuantityService.java
+++ src/main/java/com/at0m/productmanagement/service/GetAvailableQuantityService.java
@@ -1,10 +1,15 @@
 package com.at0m.productmanagement.service;
 
 import com.at0m.common.model.AvailableQuantity;
+import com.at0m.common.model.Product;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 @Service
 public class GetAvailableQuantityService {
@@ -19,10 +24,22 @@ public class GetAvailableQuantityService {
         return mongoTemplate.findAll(AvailableQuantity.class);
     }
 
-    public String saveAll(List<AvailableQuantity> quantityWithProducts){
+    public List<AvailableQuantity> saveAll(List<AvailableQuantity> quantityWithProducts){
+        List<AvailableQuantity> savedObjects = new ArrayList<>();
         for(int i=0;i<quantityWithProducts.size();i++){
-            mongoTemplate.save(quantityWithProducts.get(i));
+            AvailableQuantity availableQuantity = mongoTemplate.save(quantityWithProducts.get(i));
+            savedObjects.add(availableQuantity);
         }
-        return "Saved All";
+        return savedObjects;
+    }
+
+    public AvailableQuantity getByProductName(String productName) {
+        List<AvailableQuantity> availableQuantityByName = mongoTemplate.find(query(where("productName").is(productName)), AvailableQuantity.class);
+        if(!availableQuantityByName.isEmpty()){
+            return availableQuantityByName.get(0);
+        }
+        else{
+            return null;
+        }
     }
 }
